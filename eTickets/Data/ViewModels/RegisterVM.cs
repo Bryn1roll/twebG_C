85a9c0006f6670fae065b6fa1931e116fc24ae24
Bryn1roll <golban.vladislav1@gmail.com>
2025-04-06 14:21:31 +0300

    password validation fix

--- eTickets/Data/ViewModels/RegisterVM.cs
+++ eTickets/Data/ViewModels/RegisterVM.cs
@@ -14,10 +14,14 @@ namespace eTickets.Data.ViewModels
 
         [Display(Name = "Email address")]
         [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
         public string EmailAddress { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$",
+            ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character")]
         public string Password { get; set; }
 
         [Display(Name = "Confirm password")]
